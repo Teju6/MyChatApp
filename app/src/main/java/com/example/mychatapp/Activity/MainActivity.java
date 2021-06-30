f60da286817f487e262ce3952997195bd02c28ec
Teju6 <tejaswinichukka104@gmail.com>
2021-06-30 20:33:54 +0530

    initialsetup

---
+++ app/src/main/java/com/example/mychatapp/Activity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mychatapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.example.mychatapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2500);
+    }
+
+}
+
+
